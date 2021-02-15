68319cfd74c8c4aa47011786e79dfd214d173c13
y894577 <876432008@qq.com>
2021-02-16 00:10:01 +0800

    add route

---
+++ gee/context.go
@@ -0,0 +1,78 @@
+package gee
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Context struct {
+	//原始结构
+	Writer http.ResponseWriter
+	Req    *http.Request
+	//request信息
+	Path   string
+	Method string
+	//response信息
+	StatusCode int
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+
+//获取URL中?后面的请求参数
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+//解析GET请求中的参数
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+//设置响应status代码
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+//设置响应头
+func (c *Context) setHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+//string返回格式
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.setHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+//json返回格式
+func (c *Context) JSON(code int, obj interface{}) {
+	c.setHeader("Content-Type", "application/json")
+	c.Status(code)
+	encode := json.NewEncoder(c.Writer)
+	if err := encode.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+//data返回格式
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+//html返回格式
+func (c *Context) HTML(code int, html string) {
+	c.setHeader("Content-Type", "application/json")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
