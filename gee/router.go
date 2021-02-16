93585386ac317d2893984eaa7d2a5f9103f004d9
y894577 <876432008@qq.com>
2021-02-16 23:43:35 +0800

    update route

--- gee/router.go
+++ gee/router.go
@@ -1,30 +1,61 @@
 package gee
 
 import (
-	"log"
 	"net/http"
+	"strings"
 )
 
 //路由
 
 type router struct {
+	roots    map[string]*node
 	handlers map[string]HandlerFunc
 }
 
 func newRouter() *router {
-	return &router{handlers: make(map[string]HandlerFunc)}
+	return &router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
+}
+
+// 分割pattern
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
 }
 
 func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
-	log.Printf("Route %4s - %s", method, pattern)
+	parts := parsePattern(pattern)
+
 	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	//没有route，新建node
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	//插入
+	r.roots[method].insert(pattern, parts, 0)
 	r.handlers[key] = handler
+
 }
 
 func (r *router) handle(c *Context) {
-	key := c.Method + "-" + c.Path
-	if handler, ok := r.handlers[key]; ok {
-		handler(c)
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + c.Path
+		r.handlers[key](c)
 	} else {
 		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
 	}
--- gee/trie.go
+++ gee/trie.go
@@ -3,10 +3,15 @@ package gee
 import "strings"
 
 type node struct {
-	pattern  string  // 待匹配路由，例如 /p/:lang
-	part     string  // 路由中的一部分，例如 :lang
-	children []*node // 子节点，例如 [doc, tutorial, intro]
-	isWild   bool    // 是否精确匹配，part 含有 : 或 * 时为true
+	// 待匹配路由，例如 /p/:lang，只有在最后一层存储
+	// 搜索时到达最后一层并且pattern不为空则匹配成功
+	pattern string
+	// 路由中的一部分，例如 :lang
+	part string
+	// 子节点，例如 [doc, tutorial, intro]
+	children []*node
+	// 是否精确匹配，part 含有 : 或 * 时为true
+	isWild bool
 }
 
 //第一个匹配成功的节点，用于insert
@@ -32,7 +37,7 @@ func (n *node) matchChildren(part string) []*node {
 
 //parts []string 匹配规则
 func (n *node) insert(pattern string, parts []string, height int) {
-	//最后一层，为pattern赋值
+	//到达插入高度，为pattern赋值
 	if len(parts) == height {
 		n.pattern = pattern
 		return
@@ -41,18 +46,64 @@ func (n *node) insert(pattern string, parts []string, height int) {
 	part := parts[height]
 	//获取第一个匹配成功的节点
 	child := n.matchChild(part)
-	//该节点不匹配
+	//该节点不匹配，没有匹配到当前part的节点
 	if child == nil {
 		//插入新的节点，没有为pattern赋值
 		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
-		n.children = append(n.children, child)
+		n.children = append(n.children, child) //父节点的children插入新建的子节点
 	}
 	//递归调用
 	child.insert(pattern, parts, height+1)
 }
 
 func (n *node) search(parts []string, height int) *node {
+	//1、匹配到了第len(parts)层节点 2、*通配符
 	if len(parts) == height || strings.HasPrefix(n.part, "*") {
-
+		//如果pattern为空，匹配失败（对应情况1失败）
+		if n.pattern == "" {
+			return nil
+		}
+		//否则返回第n层节点（情况1：匹配成功，情况2：通配）
+		return n
 	}
+
+	part := parts[height]
+	//获取匹配成功的节点列表
+	children := n.matchChildren(part)
+
+	for _, child := range children {
+		result := child.search(parts, height+1)
+		if result != nil {
+			return result
+		}
+	}
+	return nil
+}
+
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+
+	if !ok {
+		return nil, nil
+	}
+
+	n := root.search(searchParts, 0) //查找是否匹配成功
+
+	if n != nil { //n不为空，匹配成功
+		parts := parsePattern(n.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[index]
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+
+	return nil, nil
 }
